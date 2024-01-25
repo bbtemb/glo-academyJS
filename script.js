d93b7022a8165a89599a844b07fc8aed862ddf40
bbtemb <bbtemb@gmail.com>
2024-01-26 01:13:47 +0500

    Добавил новые переменные, добавил вывод текста в модальном окне и в консоли

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Сообщение с любым текстом');
+console.log('Сообщение в консоли с любым текстом');
+
+let title = 'Заголовок';
+let screens = 'Дисплеи';
+let screenPrice = 'Цена';
+let rollback = 'Откат';
+let fullPrice = 'Полная цена';
+let adaptive = 'Адаптив';
