b3817bdedbaaf86fd9f466a6fbbcdce29ba7332b
bbtemb <bbtemb@gmail.com>
2024-01-26 20:38:19 +0500

    Сделал задание урока 2

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-alert('Сообщение с любым текстом');
-console.log('Сообщение в консоли с любым текстом');
+let title = 'Проект';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1000;
+let rollback = 20;
+let fullPrice = 10000;
+let adaptive = true;
 
-let title = 'Заголовок';
-let screens = 'Дисплеи';
-let screenPrice = 'Цена';
-let rollback = 'Откат';
-let fullPrice = 'Полная цена';
-let adaptive = 'Адаптив';
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов', screenPrice, 'рублей');
+console.log('Стоимость разработки сайта', fullPrice, 'рублей');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(fullPrice * (rollback / 100));
